2fc599b921c23aec0f541ec01ab500b7ace18e40
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-04 13:40:40 -0800

    Add create and update in user cont

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,32 @@
 class Api::UsersController < ApplicationController
-    def create
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            render :show
+        else
+            render :json [@user.errors.full_messages], status 422
+        end
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(change_params)
+            render :show
+        else
+            render :json [@user.errors.full_messages]
+        end
+        
     end
 
     private 
 
     def user_params
         params.require(:user).permit(:work, :education, :about_me, :first_name, :last_name, :sex, :email, :date_of_birth)
+    end
+
+    def change_params
+        params.require(:user).permit(:work, :education, :about_me)
     end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-
+Jbuiler.key_format camelize: :lower
 # Initialize the Rails application.
 Rails.application.initialize!
