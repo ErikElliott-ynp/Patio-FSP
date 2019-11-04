dcb9658e1379127735c27ed29099d519b7052ff7
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-04 13:34:12 -0800

    Add strong params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::UsersController < ApplicationController
+    def create
+
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:work, :education, :about_me, :first_name, :last_name, :sex, :email, :date_of_birth)
+    end
 end
---
+++ app/views/api/sessions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :first_name, :last_name, :sex, :date_of_birth, :email, :work, :education, :about_me
