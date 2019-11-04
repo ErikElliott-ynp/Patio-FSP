891647f2e718cf879f484f0ea3a716c13e48ee4b
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-04 14:35:32 -0800

    Add ajax methods

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -7,5 +7,16 @@ export const signup = user => {
 }
 
 export const login = user => {
-    
+    return $.ajax({
+        url: `/api/session`,
+        method: 'POST',
+        data: { user }
+    })
+}
+
+export const logout = () => {
+    return $.ajax({
+        url: `/api/session`,
+        method: 'DELETE'
+    })
 }
