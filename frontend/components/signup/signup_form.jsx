1bd3795dea59c9916d8c6a8a688e5eafb2281c10
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-05 15:09:19 -0800

    Add splah styling

--- frontend/components/signup/signup_form.jsx
+++ frontend/components/signup/signup_form.jsx
@@ -30,19 +30,26 @@ class SignupForm extends React.Component{
     render(){
         return (
             <div className="signup-max">
-                <div className="img-slogan">
-                    <h2>Connect with friends and family on The Patio.</h2>
-                    <div className="iconic">
-                        
+                <div className="left-side">
+                    <h2 className="outline">Your Friends are all here</h2>
+                    <div className="img-slogan">
+                        <div className="iconic">
+                            <h3 className="outline tagline">Connect with friends and family on The Patio.</h3>
+                            <h3 className="outline hook"><strong>See Photos and Updates</strong>from friends and <strong>Share what's new </strong>in your life</h3>
+                        </div>
                     </div>
                 </div>
                 <div className="signup-info">
+                    <h2>Sign Up</h2>
+                    <p>It's quick and easy</p>
                     <form onSubmit={this.handleSubmit}>
-                        <input onChange={this.handleChange('firstName')} type="text" placeholder="First Name" value={this.state.firstName}/>
-                        <input onChange={this.handleChange('lastName')} type="text" placeholder="Last Name" value={this.state.lastName}/>
+                        <div className="signup-names">
+                            <input onChange={this.handleChange('firstName')} type="text" placeholder="First Name" value={this.state.firstName}/>
+                            <input onChange={this.handleChange('lastName')} type="text" placeholder="Last Name" value={this.state.lastName}/>
+                        </div>
                         <input onChange={this.handleChange('email')} type="text" placeholder="Email" value={this.state.email}/>
                         <input onChange={this.handleChange('password')} type="password" placeholder="New Password" value={this.state.password}/>
-                        <input type="date" name="" id="bday" className="bday-box"/>
+                        <input type="date" name="" id="bday" className="bday-box" />
 
                     </form>
                 </div>
