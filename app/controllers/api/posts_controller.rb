dfa3aec12edbecad8c49b6629727e5a30a05ce5a
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-07 11:15:34 -0800

    Add partials for posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,4 +1,25 @@
 class Api::PostsController < ApplicationController
 
-    def
+    def index 
+        @posts = Post.where('author_id = ?', current_user.id)    
+        render :index
+    end
+
+    def show
+        @post = Post.find_by(id: params[:id])
+        if @post
+            render :show
+        else
+            render json: ["Post not found"], status: 404
+        end
+    end
+
+    def create
+
+    end
+
+    private
+
+    def post_params
+
 end
---
+++ app/views/api/posts/index.json.jbuilder
@@ -0,0 +1,3 @@
+@posts.each do |post|
+    json.partial! 'post', post: post
+end
