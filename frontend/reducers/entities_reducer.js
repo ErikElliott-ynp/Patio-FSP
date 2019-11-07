efb2172cdb40482aa0e7c5710dc5cd50ca3f85b1
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-07 14:51:11 -0800

    Create posts reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import UsersReducer from "./users_reducer";
-import SessionReducer from "./session_reducer"
+import PostReducer from "./posts_reducer";
 
 
 const EntitiesReducer = combineReducers({
-    users: UsersReducer
+    users: UsersReducer,
+    posts: PostReducer
 })
 
 export default EntitiesReducer;
