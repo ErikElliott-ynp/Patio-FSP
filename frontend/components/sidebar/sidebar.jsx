c018e90eacbbdc4060099d273dc385bf303bf5dc
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-08 11:17:52 -0800

    Style side bar

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom"
 const Sidebar = (props) => {
     return (
         <div className="feed-side-main">
-            <span> <img src="#" alt=""/> {props.currentUser.firstName}  {props.currentUser.lastName}</span>
-            <Link to="/feed">
-                <span><i class="fas fa-newspaper"></i> News Feed</span>
+            <span> <img src="#" /> {props.currentUser.firstName}  {props.currentUser.lastName}</span>
+            <Link to="/feed" className="active-news-feed">
+                <span><i className="fas fa-newspaper"></i> News Feed</span>
             </Link>
-            
-            <a href="https://www.linkedin.com/in/erik-elliott/">
-                <span><i className="fab fa-linkedin-in"></i> LinkedIn</span>
-            </a>
-            <a href="https://github.com/ErikElliott-ynp">
-                <span><i class="fab fa-github"></i> Github</span>
-            </a>
-            <a href="https://github.com/ErikElliott-ynp/Patio-FSP">
-                <span><i class="fas fa-code-branch"></i> Patio Repo</span>
-            </a>
+            <div className="info-links">
+                <a href="https://www.linkedin.com/in/erik-elliott/">
+                    <span><i className="fab fa-linkedin-in"></i>    LinkedIn</span>
+                </a>
+                <a href="https://github.com/ErikElliott-ynp">
+                    <span><i className="fab fa-github"></i>    Github</span>
+                </a>
+                <a href="https://github.com/ErikElliott-ynp/Patio-FSP">
+                    <span><i className="fas fa-code-branch"></i>    Patio Repo</span>
+                </a>
+
+            </div>
            
         </div>
     )
