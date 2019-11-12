785f8e8d0ba6438b2145b614fd4dfcccd6c5016b
ErikElliottCreates <erikelliott92@gmail.com>
2019-11-12 00:42:35 -0800

    Add modal reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,12 +3,13 @@ import { combineReducers } from "redux";
 import ErrorsReducer from "./errors_reducer";
 import EntitiesReducer from "./entities_reducer";
 import SessionReducer from "./session_reducer";
-import uiReducader from "./ui_reducer";
+import uiReducder from "./ui_reducer";
 
 const RootReducer = combineReducers({
     entities: EntitiesReducer,
     session: SessionReducer,
-    errors: ErrorsReducer
+    errors: ErrorsReducer,
+    ui: uiReducder
 })
 
 export default RootReducer;
