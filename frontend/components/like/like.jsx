c6b000a3ee17b2dcbd95c9a46702fed1a942a2bb
ErikElliottCreates <erikelliott92@gmail.com>
2019-12-11 11:39:27 -0800

    fix like css

--- frontend/components/like/like.jsx
+++ frontend/components/like/like.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Like = (props) => {
     let num = props.isLiked ? (props.likes.length - 1) : props.likes.length > 0 ? props.likes.length : null;
-    let intro = props.isLiked ? props.likes.length > 1 ? `You and ${num}` : props.likes[0].likeableType === "Post" ? `${props.currentUser.firstName}  ${props.currentUser.lastName}` : "You" : num;
+    let intro = props.isLiked ? props.likes.length > 1 ? `You and ${num}  ` : props.likes[0].likeableType === "Post" ? `${props.currentUser.firstName}  ${props.currentUser.lastName}` : "You  " : num;
     return (
         <div className="like-cont">
             <div className="like-icon"></div>
