c6fd2f52b33fb3f90bed5d07620fea6e2c154623
ErikElliottCreates <erikelliott92@gmail.com>
2019-12-11 12:29:52 -0800

    add friend request api calls

--- frontend/util/friend_request_api_util.js
+++ frontend/util/friend_request_api_util.js
@@ -1,7 +1,21 @@
-export const createFriendRequest = friend_request => {
+export const createFriendRequest = friendRequest => {
     return $.ajax({
-        url: '/api/friend_requests',
+        url: `/api/friend_requests`,
         method: 'POST',
-        data: { friend_request }
+        data: { friend_request: friendRequest }
+    })
+}
+
+export const acceptFriendRequest = friendRequestId => {
+    return $.ajax({
+        url: `/api/friend_request/${friendRequestId}`,
+        method: 'PATCH'
+    })
+}
+
+export const denyFriendRequest = friendRequestId => {
+    return $.ajax({
+        url: `/api/friend_request/${friendRequestId}`,
+        method: 'DELETE'
     })
 }
