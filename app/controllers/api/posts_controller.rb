99d8c3cba3762d7114d5acac94f21c87ca7c6f9c
erikelliott-ynp <erikelliott92@gmail.com>
2019-12-18 17:11:21 -0800

    fix css

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -7,9 +7,9 @@ class Api::PostsController < ApplicationController
     def index 
         user = params.dig(:user)
         if user == "all"
-            @posts = Post.includes(:likes, comments: [:likes] ).all
+            @posts = Post.with_attached_photo.includes(:likes, comments: [:likes] ).all
         else
-            @posts = Post.includes(:likes, comments: [:likes] )
+            @posts = Post..with_attached_photo.includes(:likes, comments: [:likes] )
                 .where('profile_id = ? OR author_id = ?', user, current_user.id)    
         end
         render :index
